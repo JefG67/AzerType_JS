5cc617a082b67a75c39ad0679cf530d3222ac7e6
JefG67 <jefguilpain@gmail.com>
2024-02-25 18:37:14 +0100

    reregarder cour sur les boucles

--- js/main.js
+++ js/main.js
@@ -1,21 +1,32 @@
-let  listeMots = ["Cachalot", "Pétunia","Serviette"]
-let listePhrases = ["Pas de panique!","La vie et l'univers","Merci pour le poisson"]
+let listeMots = ["Cachalot", "Pétunia", "Serviette"]
+let listePhrases = ["Pas de panique!", "La vie et l'univers", "Merci pour le poisson"]
 let score = 0
 
 
 
-let choixUtilisateur = prompt(" veuiller choisir ")
-
-
-
-for(i = 0; i <listeMots.length; i++ ){
-   let motUtilisateur = prompt("Entrez le mot " + listeMots [i])
-    if (motUtilisateur === listeMots[i]){
-        score = score + 1
-    }
+let choixUtilisateur = prompt(" veillez choisir  la liste  mots ou phrases")
+while (choixUtilisateur !== "mots" && choixUtilisateur !== "phrases") {
+    choixUtilisateur = prompt(" veillez choisir  la liste  mots ou phrases")
 }
 
-console.log("votre score est de " + score + " sur" + listeMots.length)
+if (choixUtilisateur == "mots") {
+
+    for (i = 0; i < listeMots.length; i++) {
+        let motUtilisateur = prompt("Entrez le mot " + listeMots[i])
+        if (motUtilisateur === listeMots[i]) {
+            score = score + 1
+        }
+    }
+    console.log("votre score est de " + score + " sur" + listeMots.length)
+} else {
+    for (i = 0; i < listePhrases.length; i++) {
+        let motUtilisateur = prompt("Entrez la phrase " + listePhrases[i])
+        if (motUtilisateur === listePhrases[i]) {
+            score = score + 1
+        }
+    }
+    console.log("votre score est de " + score + " sur" + listePhrases.length)
+}
 
 
 
