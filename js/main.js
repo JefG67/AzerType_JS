d77ad21fb7acf8b643703dc831f2e91cc1471e96
JefG67 <jefguilpain@gmail.com>
2024-02-27 21:54:14 +0100

    recuperez un element d'une page web

--- js/main.js
+++ js/main.js
@@ -1,39 +1,52 @@
 let listeMots = ["Cachalot", "Pétunia", "Serviette"]
 let listePhrases = ["Pas de panique!", "La vie et l'univers", "Merci pour le poisson"]
-let score = 0
 
 
 
-let choixUtilisateur = prompt(" veillez choisir  la liste  mots ou phrases")
-while (choixUtilisateur !== "mots" && choixUtilisateur !== "phrases") {
-    choixUtilisateur = prompt(" veillez choisir  la liste  mots ou phrases")
-}
-
-if (choixUtilisateur == "mots") {
-
-    for (i = 0; i < listeMots.length; i++) {
-        let motUtilisateur = prompt("Entrez le mot " + listeMots[i])
-        if (motUtilisateur === listeMots[i]) {
-            score = score + 1
-        }
-    }
-    console.log("votre score est de " + score + " sur" + listeMots.length)
-} else {
-    for (i = 0; i < listePhrases.length; i++) {
-        let motUtilisateur = prompt("Entrez la phrase " + listePhrases[i])
-        if (motUtilisateur === listePhrases[i]) {
-            score = score + 1
-        }
-    }
-    console.log("votre score est de " + score + " sur" + listePhrases.length)
-}
-
-
-function retournerMessageScore(score, nombreQuestion){
-    let message = "votre score est de " + score + " sur " +nombreQuestion
+function retournerMessageScore(score, nombreQuestion) {
+    let message = "votre score est de " + score + " sur " + nombreQuestion
     return message
 }
 
 
-let nouveauMessage = retournerMessageScore(5, 10)
-console.log(nouveauMessage)
+function afficherResultat(score, nombreMotsProposes) {
+
+    console.log("votre score est de " + score + " sur " + nombreMotsProposes)
+
+}
+
+function choisirPhraseOuMots() {
+    let choixUtilisateur = prompt(' Voulez vous jouer avec les mots (entrez "mots")ou les phrases (entrez "phrases"?')
+    while (choixUtilisateur !== "mots" && choixUtilisateur !== "phrases") {
+        choixUtilisateur = prompt(" veuillez choisir  la liste  mots ou phrases")
+    }
+    return choixUtilisateur
+}
+
+function lancerBoucleDeJeu(listeProposition) {
+    let score = 0
+    for (i = 0; i < listeProposition.length; i++) {
+        let motUtilisateur = prompt("Entrez le mot " + listeProposition[i])
+        if (motUtilisateur === listeProposition[i]) {
+            score = score + 1
+        }
+
+    }
+    return score
+}
+function lancerJeu(){
+    let choix = choisirPhraseOuMots()
+    let score = 0
+    let nombreMotsProposes = 0
+
+    if (choix === 'mots'){
+        score = lancerBoucleDeJeu(listeMots)
+        nombreMotsProposes = listeMots.length
+    } else {
+        score = lancerBoucleDeJeu(listePhrases)
+        nombreMotsProposes = listePhrases.length
+    }
+    afficherResultat(score, nombreMotsProposes)
+}
+
+lancerJeu();
